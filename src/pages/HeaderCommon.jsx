298de8f2d14c1b47add5e41e32c2fb719f577231
ShabiGardezi <shabigardezi51214@gmail.com>
2023-08-24 16:03:09 -0500

    HeaderCommon

---
+++ src/pages/HeaderCommon.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import HeaderCommonBG from "../assests/header-bg.png";
+import calender from "../assests/78f5f7ff2e2e338d72cdaf158205ac8d.png";
+import "../styles/HeaderCommon.css";
+function HeaderCommon({ title }) {
+  const currentDate = new Date();
+  const dateTimeString = currentDate.toLocaleString();
+  return (
+    <div className="HeaderCommon">
+      <div className="Header-container">
+        <div
+          className="HeaderBackground"
+          style={{
+            backgroundImage: `url(${HeaderCommonBG})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="header-content">
+            <h2 className="heading">{title}</h2>
+            <div className="date-time">
+              <div className="date">
+                <img className="time" src={calender} alt="calender" />
+              </div>
+              <p className="calendar">{dateTimeString}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderCommon;
--- src/pages/ZipCodeDataDisplay.jsx
+++ src/pages/ZipCodeDataDisplay.jsx
@@ -1,5 +1,3 @@
-// src/ProviderForm.js
-
 import React, { useState } from "react";
 import {
   TextField,
@@ -8,10 +6,9 @@ import {
   Typography,
   Box,
   Container,
-  IconButton,
 } from "@mui/material";
-import { AddCircleOutline as AddCircleOutlineIcon } from "@mui/icons-material";
 import "../styles/ProviderForm.css";
+import HeaderCommon from "./HeaderCommon";
 function ProviderForm() {
   const [providerName, setProviderName] = useState("");
   const [zipCode, setZipCode] = useState("");
@@ -19,96 +16,93 @@ function ProviderForm() {
   const [features, setFeatures] = useState("");
   const [price, setPrice] = useState("");
   const [rating, setRating] = useState(null);
+  const [channel, setChannel] = useState("");
 
-  const handleRatingChange = (event, newValue) => {
+  const handleRatingChange = (_event, newValue) => {
     setRating(newValue);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Handle form submission logic here
   };
 
   return (
-    <Container maxWidth="sm">
-      {/* <div className="provider-header">
-        <h2>Zip Code</h2>
-        <div className="btn-add">
-          <button className="add-now">Add Now</button>
-        </div>
-      </div> */}
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Provider Name"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={providerName}
-          onChange={(e) => setProviderName(e.target.value)}
-        />
-
-        <TextField
-          label="Zip Code"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={zipCode}
-          onChange={(e) => setZipCode(e.target.value)}
-        />
-        <TextField
-          label="Max Speed"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={maxSpeed}
-          onChange={(e) => setMaxSpeed(e.target.value)}
-        />
-        <TextField
-          label="Channels"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={providerName}
-          onChange={(e) => setProviderName(e.target.value)}
-        />
-        <TextField
-          label="Features"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={features}
-          onChange={(e) => setFeatures(e.target.value)}
-        />
-        <TextField
-          label="Price"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <Typography variant="h6" gutterBottom>
-          Rating
-        </Typography>
-        <Box display="flex" alignItems="center">
-          <Rating
-            name="rating"
-            value={rating}
-            onChange={handleRatingChange}
-            precision={0.1}
-            size="large"
+    <>
+      <HeaderCommon title="Zip Code" />
+      <Container maxWidth="sm">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Provider Name"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            value={providerName}
+            onChange={(e) => setProviderName(e.target.value)}
           />
-          {rating !== null && (
-            <Typography variant="h6" style={{ marginLeft: "10px" }}>
-              {rating}
-            </Typography>
-          )}
-        </Box>
-        <Button variant="contained" color="primary" type="submit">
-          Submit
-        </Button>
-      </form>
-    </Container>
+
+          <TextField
+            label="Zip Code"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            value={zipCode}
+            onChange={(e) => setZipCode(e.target.value)}
+          />
+          <TextField
+            label="Max Speed"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            value={maxSpeed}
+            onChange={(e) => setMaxSpeed(e.target.value)}
+          />
+          <TextField
+            label="Channels"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            value={channel}
+            onChange={(e) => setChannel(e.target.value)}
+          />
+          <TextField
+            label="Features"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            value={features}
+            onChange={(e) => setFeatures(e.target.value)}
+          />
+          <TextField
+            label="Price"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <Typography variant="h6" gutterBottom>
+            Rating
+          </Typography>
+          <Box display="flex" alignItems="center">
+            <Rating
+              name="rating"
+              value={rating}
+              onChange={handleRatingChange}
+              precision={0.1}
+              size="large"
+            />
+            {rating !== null && (
+              <Typography variant="h6" style={{ marginLeft: "10px" }}>
+                {rating}
+              </Typography>
+            )}
+          </Box>
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Container>
+    </>
   );
 }
 
