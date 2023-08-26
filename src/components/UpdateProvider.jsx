4ac8eb7eac1b72b0edd3cb6ccc192317ccf289a8
shabigardezi <shabigardezi51214@gmail.com>
2023-08-26 07:17:29 +0500

    update provider component

--- src/components/UpdateProvider.jsx
+++ src/components/UpdateProvider.jsx
@@ -12,6 +12,7 @@ import {
 } from "@mui/material";
 import "../styles/ProviderForm.css";
 import HeaderCommon from "../pages/HeaderCommon";
+import VerticalNavbar from "./Sidebar";
 function ProviderForm() {
   const [providerName, setProviderName] = useState("");
   const [zipCode, setZipCode] = useState("");
@@ -94,101 +95,104 @@ function ProviderForm() {
   };
 
   return (
-    <div className="Provider-container">
-      <HeaderCommon title="Update Zip Code" />
-      <Container maxWidth="sm">
-        <h2 className="heading-1">Update Provider</h2>
-        <form onSubmit={handleSubmit}>
-          {/* <InputLabel>Provider Name</InputLabel>
+    <>
+      <VerticalNavbar />
+      <div className="Provider-container">
+        <HeaderCommon title="Update Zip Code" />
+        <Container maxWidth="sm">
+          <h2 className="heading-1">Update Provider</h2>
+          <form onSubmit={handleSubmit}>
+            {/* <InputLabel>Provider Name</InputLabel>
             <Select
-                value={providerName}
-                onChange={(e) => setProviderName(e.target.value)}
+            value={providerName}
+            onChange={(e) => setProviderName(e.target.value)}
             >
                 <MenuItem value="" disabled>
                 Choose Provider
                 </MenuItem>
                 {companies.map((company, index) => (
-                <MenuItem key={index} value={company}>
+                    <MenuItem key={index} value={company}>
                     {company}
-                </MenuItem>
-                ))}
+                    </MenuItem>
+                    ))}
             </Select> */}
-          <TextField
-            select
-            label="Provider Name"
-            value={providerName}
-            onChange={(e) => setProviderName(e.target.value)}
-            fullWidth
-          >
-            {companies.map((company, index) => (
-              <MenuItem key={index} value={company}>
-                {company}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            label="Zip Code"
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            value={zipCode}
-            onChange={(e) => setZipCode(e.target.value)}
-          />
-          <TextField
-            label="Max Speed"
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            value={maxSpeed}
-            onChange={(e) => setMaxSpeed(e.target.value)}
-          />
-          <TextField
-            label="Channels"
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            value={channel}
-            onChange={(e) => setChannel(e.target.value)}
-          />
-          <TextField
-            label="Features"
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            value={features}
-            onChange={(e) => setFeatures(e.target.value)}
-          />
-          <TextField
-            label="Price"
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-          <Typography variant="h6" gutterBottom>
-            Rating
-          </Typography>
-          <Box display="flex" alignItems="center">
-            <Rating
-              name="rating"
-              value={rating}
-              onChange={handleRatingChange}
-              precision={0.1}
-              size="large"
+            <TextField
+              select
+              label="Provider Name"
+              value={providerName}
+              onChange={(e) => setProviderName(e.target.value)}
+              fullWidth
+            >
+              {companies.map((company, index) => (
+                <MenuItem key={index} value={company}>
+                  {company}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="Zip Code"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
             />
-            {rating !== null && (
-              <Typography variant="h6" style={{ marginLeft: "10px" }}>
-                {rating}
-              </Typography>
-            )}
-          </Box>
-          <Button variant="contained" color="primary" type="submit">
-            Add Now
-          </Button>
-        </form>
-      </Container>
-    </div>
+            <TextField
+              label="Max Speed"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={maxSpeed}
+              onChange={(e) => setMaxSpeed(e.target.value)}
+            />
+            <TextField
+              label="Channels"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={channel}
+              onChange={(e) => setChannel(e.target.value)}
+            />
+            <TextField
+              label="Features"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={features}
+              onChange={(e) => setFeatures(e.target.value)}
+            />
+            <TextField
+              label="Price"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <Typography variant="h6" gutterBottom>
+              Rating
+            </Typography>
+            <Box display="flex" alignItems="center">
+              <Rating
+                name="rating"
+                value={rating}
+                onChange={handleRatingChange}
+                precision={0.1}
+                size="large"
+              />
+              {rating !== null && (
+                <Typography variant="h6" style={{ marginLeft: "10px" }}>
+                  {rating}
+                </Typography>
+              )}
+            </Box>
+            <Button variant="contained" color="primary" type="submit">
+              Add Now
+            </Button>
+          </form>
+        </Container>
+      </div>
+    </>
   );
 }
 
